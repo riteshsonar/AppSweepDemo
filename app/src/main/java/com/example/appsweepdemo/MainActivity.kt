d3e25f0ac9dea0005da25f8ad80f28b3786233dc
Ritesh <ritesh.sonar111@gmail.com>
2024-04-23 00:59:49 +0530

    remove print fun

--- app/src/main/java/com/example/appsweepdemo/MainActivity.kt
+++ app/src/main/java/com/example/appsweepdemo/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    println("Hello World")
                     Greeting("Android")
                 }
             }
