219084795350d2b1a782476a0e9217202a0dbe7b
Ritesh <ritesh.sonar111@gmail.com>
2024-04-23 13:38:52 +0530

    change in gradle file

--- app/src/main/java/com/example/appsweepdemo/MainActivity.kt
+++ app/src/main/java/com/example/appsweepdemo/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    println("working with AppSweep")
+                    println("Hello im working with AppSweep")
                     Greeting("Android")
                 }
             }
