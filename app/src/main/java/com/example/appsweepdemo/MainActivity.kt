655bf86fe633be3cbd79e1e769c4509256cbde24
Ritesh <ritesh.sonar111@gmail.com>
2024-04-23 16:06:31 +0530

    message change

--- app/src/main/java/com/example/appsweepdemo/MainActivity.kt
+++ app/src/main/java/com/example/appsweepdemo/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    println("Hello im working with AppSweep")
+                    println("have a great day")
                     Greeting("Android")
                 }
             }
