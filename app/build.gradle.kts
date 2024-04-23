d2349593b92e5c736045193b875a1b6a3ad239c8
Ritesh <ritesh.sonar111@gmail.com>
2024-04-23 13:35:08 +0530

    change in gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.appsweepdemo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.appsweepdemo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
